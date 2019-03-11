97e2cdf57b4ef991938217e388366f209396908f
nicolas199818 <nthibault@myges.fr>
2019-03-11 16:04:31 +0100

    requête Post

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express')
-const app = express()
+const app = express();
+var bodyParser = require('body-parser')
+
+app.use(express.json());
 
 app.get('/hello', function (req, res) {
-  res.send('Hello World!')
+  res.send('Hello World!');
+})
+
+app.post('/chat',function(req,res){
+	if(req.body.msg == 'meteo'){
+		res.send('Il fait beau')
+	}
+	if(req.body.msg == 'ville'){
+		res.send('Nous sommes à Paris')
+	}
 })
 
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 3000!');
 })
