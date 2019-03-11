cb8ee2e532c29ec8a9a711e1181968a47029341d
nicolas199818 <nthibault@myges.fr>
2019-03-11 14:47:06 +0100

    Avant Heroku

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-alert('Hello World');
+const express = require('express')
+const app = express()
+
+app.get('/hello', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
